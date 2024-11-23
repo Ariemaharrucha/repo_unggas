9b8d7281a0ad8e2b2f4c95b16c1477394e97ee9e
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-23 14:52:12 +0700

    add edit user profile

--- BE_V2/src/services/user.service.js
+++ BE_V2/src/services/user.service.js
@@ -7,6 +7,25 @@ const userService = {
     return users;
   },
 
+  getUserById: async (id) => {
+    const result = await userModel.getUserById(id);
+    if(result == false) {
+      throw new Error("user not found");
+    }
+    return result;
+  },
+
+  editUserProfile: async (id, data) => {
+    const { username, email, password, image_profile } = data;
+    if (!username || !email || !password) {
+      throw new Error("username, email, dan password harus diisi.");
+    };
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await userModel.editUser(id, { username, email, password: hashedPassword, image_profile });
+    return  result
+  },
+
   createAdmin: async (data) => {
     const {
       username,
