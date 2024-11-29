98ccb8dd23f4a3a982cb34bb94f4238f509f3d47
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-11-29 22:58:07 +0700

    add timeout

--- BE_V2/src/config/cloudinary.js
+++ BE_V2/src/config/cloudinary.js
@@ -8,6 +8,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
+  timeout: 60000
 });
 
 export default cloudinary;
