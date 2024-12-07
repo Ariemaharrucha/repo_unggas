6c14c6e67fc44cda9af7ab7f39187ff9414b3773
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-12-07 22:32:48 +0700

    edit getDokter and add delete dokter

--- BE_V2/src/model/dokter.model.js
+++ BE_V2/src/model/dokter.model.js
@@ -7,10 +7,15 @@ const dokterModel = {
       `SELECT 
          u.user_id AS dokter_id, 
          u.username AS nama_dokter, 
+         u.email,
          u.image_profile, 
          d.spesialis, 
          d.pengalaman, 
-         d.jam_kerja 
+         d.jam_kerja,
+         d.nomer_telepon,
+         d.nomer_str,
+         d.alumni,
+         d.tempat_praktek
        FROM dokter d
        JOIN users u ON d.dokter_id = u.user_id
        WHERE u.role = 'dokter'`;
@@ -71,6 +76,11 @@ const dokterModel = {
     const { nomer_str, nomer_telepon, spesialis, pengalaman, jam_kerja, alumni, tempat_praktek } = data;
     const sqlQuery = `UPDATE dokter SET nomer_str = ?, nomer_telepon = ?, spesialis = ?, pengalaman = ?, jam_kerja = ?, alumni = ?, tempat_praktek = ? WHERE dokter_id = ?`;
     return query(sqlQuery, [nomer_str, nomer_telepon, spesialis, pengalaman, jam_kerja, alumni, tempat_praktek, id]);
+  },
+
+  deleteDokter: async (id) => {
+    const sqlQuery = `DELETE FROM users WHERE user_id = ?`;
+    return query(sqlQuery, [id]);
   },
 
   // getUserForDokter: async (dokterId) => {
